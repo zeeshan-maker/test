571f2452e69e60920ac1b6bdf76b33bf4cfcb56a
zeeshan-techtorch <zeeshan.khan@techtorch.solutions>
2025-07-08 09:23:57 +0530

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Get all todos
-app.get('/todos', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({status:200,message:"Welcome to Test API."});
 });
 
